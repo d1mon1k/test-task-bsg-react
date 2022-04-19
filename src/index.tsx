64655a79a50212c53af0db5d3377355df01763eb
d1mon1k <ostrovskiy.a.97@gmail.com>
2022-04-19 15:52:54 +0200

    fix: fix bugs with deeployment

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 //note pass: Test12!@ login: test@bsgroup.eu | trial or main
 
 const container = document.getElementById('app')
 const root = createRoot(container!)
 root.render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 )
